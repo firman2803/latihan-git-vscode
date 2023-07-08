73ff7c451e9f010b7a00d377a412900c466ed1df
Fire.man <mantrijombang50@gmail.com>
2023-07-08 10:20:52 +0700

    first commit

---
+++ 1. Coba Awal.py
@@ -0,0 +1,13 @@
+print("hello world")
+
+a=10
+x=5
+panjang = 1000
+print("Nilai a = ", a)
+print("Nilai x = ", x)
+print("Nilai panjang = ", panjang)
+
+#penamaan
+nilai_y = 15
+juta10 = 10000000
+
